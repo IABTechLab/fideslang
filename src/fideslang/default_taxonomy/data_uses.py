acecddad774af43fd582c9cc4c6a4c38f4dcc9c7
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-05-18 22:03:23 +0800

    feat: rework top-level and second-level uses

--- src/fideslang/default_taxonomy/data_uses.py
+++ src/fideslang/default_taxonomy/data_uses.py
@@ -1,53 +1,61 @@
 from fideslang.models import DataUse
 
 DEFAULT_DATA_USES = [
-    # Provide
+    # Essential
     DataUse(
-        fides_key="provide",
+        fides_key="essential",
+        name="TODO",
         organization_fides_key="default_organization",
-        name="Provide the capability",
         description="Provide, give, or make available the product, service, application or system.",
         parent_key=None,
         is_default=True,
     ),
     DataUse(
-        fides_key="provide.service",
+        fides_key="essential.legal_obligation",
+        organization_fides_key="default_organization",
+        name="Legal Obligation",
+        description="TODO",
+        parent_key="essential",
+        is_default=True,
+    ),
+    DataUse(
+        fides_key="essential.service",
         organization_fides_key="default_organization",
         name="Service",
         description="The source service, system, or product being provided to the user.",
-        parent_key="provide",
+        parent_key="essential",
         is_default=True,
     ),
     DataUse(
-        fides_key="provide.service.operations",
+        fides_key="essential.service.operations",
         organization_fides_key="default_organization",
         name="Service Operations",
         description="Use of specified data categories to operate and protect in order to provide the service.",
-        parent_key="provide.service",
+        parent_key="essential.service",
         is_default=True,
     ),
     DataUse(
-        fides_key="provide.service.operations.support",
+        fides_key="essential.service.operations.support",
         organization_fides_key="default_organization",
         name="Operations Support",
         description="Use of specified data categories to provide support for operation and protection in order to provide the service.",
-        parent_key="provide.service.operations",
+        parent_key="essential.service.operations",
         is_default=True,
     ),
     DataUse(
-        fides_key="provide.service.operations.support.optimization",
+        fides_key="essential.service.operations.support.optimization",
         organization_fides_key="default_organization",
         name="Support Optimization",
         description="Use of specified data categories to optimize and improve support operations in order to provide the service.",
-        parent_key="provide.service.operations.support",
+        parent_key="essential.service.operations.support",
         is_default=True,
     ),
     DataUse(
-        fides_key="provide.service.upgrades",
+        fides_key="essential.service.upgrades",
         organization_fides_key="default_organization",
         name="Offer Upgrades",
         description="Offer upgrades or upsales such as increased capacity for the service based on monitoring of service usage.",
-        parent_key="provide.service",
+        parent_key="essential.service",
         is_default=True,
     ),
     # Improve
@@ -67,6 +75,7 @@ DEFAULT_DATA_USES = [
         parent_key="improve",
         is_default=True,
     ),
+    # Personalize
     DataUse(
         fides_key="personalize",
         organization_fides_key="default_organization",
@@ -83,8 +92,17 @@ DEFAULT_DATA_USES = [
         parent_key="personalize",
         is_default=True,
     ),
+    # Marketing
     DataUse(
-        fides_key="advertising",
+        fides_key="marketing",
+        organization_fides_key="default_organization",
+        name="Marketing",
+        description="",
+        parent_key=None,
+        is_default=True,
+    ),
+    DataUse(
+        fides_key="marketing.advertising",
         organization_fides_key="default_organization",
         name="Advertising, Marketing or Promotion",
         description="The promotion of products or services targeted to users based on the the processing of user provided data in the system.",
@@ -92,45 +110,46 @@ DEFAULT_DATA_USES = [
         is_default=True,
     ),
     DataUse(
-        fides_key="advertising.first_party",
+        fides_key="marketing.advertising.first_party",
         organization_fides_key="default_organization",
         name="First Party Advertising",
         description="The promotion of products or services targeting users based on processing of derviced data from prior use of the system.",
-        parent_key="advertising",
+        parent_key="marketing.advertising",
         is_default=True,
     ),
     DataUse(
-        fides_key="advertising.third_party",
+        fides_key="marketing.advertising.third_party",
         organization_fides_key="default_organization",
         name="Third Party Advertising",
         description="The promotion of products or services targeting users based on processing of specific categories of data acquired from third party sources.",
-        parent_key="advertising",
+        parent_key="marketing.advertising",
         is_default=True,
     ),
     DataUse(
-        fides_key="advertising.first_party.contextual",
+        fides_key="marketing.advertising.first_party.contextual",
         organization_fides_key="default_organization",
         name="First Party Contextual Advertising",
         description="The promotion of products or services targeted to users based on the processing of data from the users prior use of the services.",
-        parent_key="advertising.first_party",
+        parent_key="marketing.advertising.first_party",
         is_default=True,
     ),
     DataUse(
-        fides_key="advertising.first_party.personalized",
+        fides_key="marketing.advertising.first_party.personalized",
         organization_fides_key="default_organization",
         name="First Party Personalized Advertising",
         description="The targeting and changing of promotional content based on processing of specific data categories from the user.",
-        parent_key="advertising.first_party",
+        parent_key="marketing.advertising.first_party",
         is_default=True,
     ),
     DataUse(
-        fides_key="advertising.third_party.personalized",
+        fides_key="marketing.advertising.third_party.personalized",
         organization_fides_key="default_organization",
         name="Third Party Personalized Advertising",
         description="The targeting and changing of promotional content based on processing of specific categories of user data acquired from third party sources.",
-        parent_key="advertising.third_party",
+        parent_key="marketing.advertising.third_party",
         is_default=True,
     ),
+    # Third-Party Sharing
     DataUse(
         fides_key="third_party_sharing",
         organization_fides_key="default_organization",
@@ -171,6 +190,7 @@ DEFAULT_DATA_USES = [
         parent_key="third_party_sharing",
         is_default=True,
     ),
+    # Collect
     DataUse(
         fides_key="collect",
         organization_fides_key="default_organization",
@@ -179,11 +199,57 @@ DEFAULT_DATA_USES = [
         parent_key=None,
         is_default=True,
     ),
+    # Train AI System
     DataUse(
         fides_key="train_ai_system",
         organization_fides_key="default_organization",
         name="Train AI System",
         description="Training an AI system. Please note when this data use is specified, the method and degree to which a user may be directly identified in the resulting AI system should be appended.",
+        parent_key=None,
+        is_default=True,
+    ),
+    # Analytics
+    DataUse(
+        fides_key="analytics",
+        organization_fides_key="default_organization",
+        name="Analytics",
+        description="TODO",
+        parent_key=None,
+        is_default=True,
+    ),
+    # Operations
+    DataUse(
+        fides_key="operations",
+        name="Operations",
+        organization_fides_key="default_organization",
+        description="TODO",
+        parent_key=None,
+        is_default=True,
+    ),
+    # Finance
+    DataUse(
+        fides_key="Finance",
+        name="Finance",
+        organization_fides_key="default_organization",
+        description="TODO",
+        parent_key=None,
+        is_default=True,
+    ),
+    # Employment
+    DataUse(
+        fides_key="employment",
+        name="Employment",
+        organization_fides_key="default_organization",
+        description="TODO",
+        parent_key=None,
+        is_default=True,
+    ),
+    #  Sales
+    DataUse(
+        fides_key="sales",
+        name="Sales",
+        organization_fides_key="default_organization",
+        description="TODO",
         parent_key=None,
         is_default=True,
     ),
