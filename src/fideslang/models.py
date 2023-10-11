499e96be17efe9ac8f4919c1646d6b38c4171126
Adam Sachs <adam@ethyca.com>
2023-10-11 12:43:58 -0400

    update flexible_legal_basis boolean field to be optional (#178)

--- src/fideslang/models.py
+++ src/fideslang/models.py
@@ -991,9 +991,8 @@ class PrivacyDeclaration(BaseModel):
     features: List[str] = Field(
         default_factory=list, description="The features of processing personal data."
     )
-    flexible_legal_basis_for_processing: bool = Field(
+    flexible_legal_basis_for_processing: Optional[bool] = Field(
         description="Whether the legal basis for processing is 'flexible' (i.e. can be overridden in a privacy notice) for this declaration.",
-        default=False,
     )
     legal_basis_for_processing: Optional[LegalBasisForProcessingEnum] = Field(
         description="The legal basis under which personal data is processed for this purpose."
