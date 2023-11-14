126e4befac59467f2e08654428476acd8893c755
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-11-14 15:24:21 +0800

    feat: update CI checks for new pydantic versions

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,7 @@ import nox
 nox.options.sessions = []
 nox.options.reuse_existing_virtualenvs = True
 
+# These should match what is in the `pr_checks.yml` file for CI runs
 TESTED_PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11"]
 TESTED_PYDANTIC_VERSIONS = ["2.2.1", "2.3.0", "2.4.2", "2.5.0"]
 TESTED_PYYAML_VERSIONS = ["5.4.1", "6.0"]
